e34b0a96a239c97cb4929e4e8c2ef68d9d21044a
Hiran <hiran.tassinari@gmail.com>
2019-03-06 23:54:19 -0300

    Creating first asp net core app

---
+++ WebApiTutorial1/WebApiTutorial1Webapi/Controllers/ToDoController.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebApiTutorial1Webapi.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApiTutorial1Webapi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ToDoController : ControllerBase
+    {
+        private readonly TodoContext _context;
+
+
+        public ToDoController(TodoContext context)
+        {
+            _context = context;
+
+            if (_context.TodoItems.Any())
+            {
+                return;
+            }
+
+            // Create a new TodoItem if collection is empty,
+            // which means you can't delete all TodoItems.
+            _context.TodoItems.Add(new TodoItem { Name = "Item1" });
+            _context.SaveChanges();
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<TodoItem>>> GetAllItems()
+        {
+            return await _context.TodoItems.ToListAsync();
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<TodoItem>> GetItemById(long id)
+        {
+            var todo = await _context.TodoItems.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            return todo;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<TodoItem>> SaveToDoItem(TodoItem item)
+        {
+            await _context.TodoItems.AddAsync(item);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetItemById), routeValues: new { id = item.Id }, value: item);
+        }
+
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutToDoItem(long id, TodoItem item)
+        {
+            if (id != item.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(item).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteToDoItem(long id)
+        {
+            var todo = await _context.TodoItems.FindAsync(id);
+
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            _context.TodoItems.Remove(todo);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ WebApiTutorial1/WebApiTutorial1Webapi/wwwroot/site.js
@@ -0,0 +1,134 @@
+﻿const uri = "api/todo";
+let todos = null;
+function getCount(data) {
+    const el = window.$("#counter");
+    let name = "to-do";
+    if (data) {
+        if (data > 1) {
+            name = "to-dos";
+        }
+        el.text(data + " " + name);
+    } else {
+        el.text(`No ${name}`);
+    }
+}
+
+$(document).ready(function () {
+    getData();
+});
+
+function getData() {
+    window.$.ajax({
+        type: "GET",
+        url: uri,
+        cache: false,
+        success: function (data) {
+            const tBody = window.$("#todos");
+
+            window.$(tBody).empty();
+
+            getCount(data.length);
+
+            window.$.each(data, function (key, item) {
+                const tr = window.$("<tr></tr>")
+                    .append(
+                        window.$("<td></td>").append(
+                            window.$("<input/>", {
+                                type: "checkbox",
+                                disabled: true,
+                                checked: item.isComplete
+                            })
+                        )
+                    )
+                    .append(window.$("<td></td>").text(item.name))
+                    .append(
+                        window.$("<td></td>").append(
+                            window.$("<button>Edit</button>").on("click", function () {
+                                editItem(item.id);
+                            })
+                        )
+                    )
+                    .append(
+                        window.$("<td></td>").append(
+                            window.$("<button>Delete</button>").on("click", function () {
+                                deleteItem(item.id);
+                            })
+                        )
+                    );
+
+                tr.appendTo(tBody);
+            });
+
+            todos = data;
+        }
+    });
+}
+
+function addItem() {
+    const item = {
+        name: window.$("#add-name").val(),
+        isComplete: false
+    };
+
+    window.$.ajax({
+        type: "POST",
+        accepts: "application/json",
+        url: uri,
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        error: function (jqXHR, textStatus, errorThrown) {
+            alert("Something went wrong!");
+        },
+        success: function (result) {
+            getData();
+            window.$("#add-name").val("");
+        }
+    });
+}
+
+function deleteItem(id) {
+    window.$.ajax({
+        url: uri + "/" + id,
+        type: "DELETE",
+        success: function (result) {
+            getData();
+        }
+    });
+}
+
+function editItem(id) {
+    window.$.each(todos, function (key, item) {
+        if (item.id === id) {
+            window.$("#edit-name").val(item.name);
+            window.$("#edit-id").val(item.id);
+            window.$("#edit-isComplete")[0].checked = item.isComplete;
+        }
+    });
+    window.$("#spoiler").css({ display: "block" });
+}
+
+$(".my-form").on("submit", function () {
+    const item = {
+        name: window.$("#edit-name").val(),
+        isComplete: window.$("#edit-isComplete").is(":checked"),
+        id: window.$("#edit-id").val()
+    };
+
+    window.$.ajax({
+        url: uri + "/" + window.$("#edit-id").val(),
+        type: "PUT",
+        accepts: "application/json",
+        contentType: "application/json",
+        data: JSON.stringify(item),
+        success: function (result) {
+            getData();
+        }
+    });
+
+    closeInput();
+    return false;
+});
+
+function closeInput() {
+    window.$("#spoiler").css({ display: "none" });
+}
